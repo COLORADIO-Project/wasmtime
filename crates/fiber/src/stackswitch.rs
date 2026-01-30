170042df9834eabb5365d3411e821239c5ce8e5f
Max Daubmann <maxdau@uni-bremen.de>
2026-01-30 18:09:49 +0100

    Added fiber implementation for riscv32

--- crates/fiber/src/stackswitch.rs
+++ crates/fiber/src/stackswitch.rs
@@ -29,6 +29,10 @@ cfg_if::cfg_if! {
         mod riscv64;
         pub(crate) use supported::*;
         pub(crate) use riscv64::*;
+    } else if #[cfg(all(target_arch = "riscv32", not(target_feature = "f")))] {
+        mod riscv32;
+        pub(crate) use supported::*;
+        pub(crate) use riscv32::*;
     } else {
         // No support for this platform. Don't fail compilation though and
         // instead defer the error to happen at runtime when a fiber is created.
